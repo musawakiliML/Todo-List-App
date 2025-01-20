952a59153856ec96b6964e40a0dabe4963ce5fb1
Musa Adamu Wakili <musawakiliml@hotmail.com>
2025-01-20 03:02:13 +0100

    Update: Add For loops for printing todo from a list

--- main.go
+++ main.go
@@ -5,8 +5,16 @@ import "fmt"
 func main() {
 
 	var todo1 = "Learning Go!"
+	var todo2 = "Building a project in Go"
+	var todo3 = "Writing a blog on each project"
 
-	fmt.Println("Hello, World!")
+	var todoItems = []string{todo1, todo2, todo3}
 
-	fmt.Println(todo1)
+	fmt.Println("#### Welcome to my Go Learning Project ####")
+
+	fmt.Println("List of My Todo Lists")
+
+	for index, task := range todoItems {
+		fmt.Println(index, task)
+	}
 }
