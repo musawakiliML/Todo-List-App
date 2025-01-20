fb83539cba40b710974a603faa0693a63f456a0d
Musa Adamu Wakili <musawakiliml@hotmail.com>
2025-01-20 02:46:04 +0100

    Update: Initial Project Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module Todo-List_App
+
+go 1.23
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var todo1 = "Learning Go!"
+
+	fmt.Println("Hello, World!")
+
+	fmt.Println(todo1)
+}
