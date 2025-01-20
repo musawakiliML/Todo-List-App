1ceb1726044ef35bf763d251568f6b68066150b8
Musa Adamu Wakili <musawakiliml@hotmail.com>
2025-01-20 03:37:27 +0100

    Refactor: Create function for resuse to display different todos category

--- main.go
+++ main.go
@@ -2,16 +2,22 @@ package main
 
 import "fmt"
 
+var todo1 = "Learning Go!"
+var todo2 = "Building a project in Go"
+var todo3 = "Writing a blog on each project"
+
+var todoItems = []string{todo1, todo2, todo3}
+
 func main() {
-
-	var todo1 = "Learning Go!"
-	var todo2 = "Building a project in Go"
-	var todo3 = "Writing a blog on each project"
-
-	var todoItems = []string{todo1, todo2, todo3}
 
 	fmt.Println("#### Welcome to my Go Learning Project ####")
 
+	// Call function for Displaying Todos
+	printTodos()
+
+}
+
+func printTodos() {
 	fmt.Println("List of My Todo Lists")
 
 	for index, task := range todoItems {
